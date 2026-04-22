4fbff73192fec41ae332a27de8c3d064707f7906
Lencho <mandeveloper582@gmail.com>
2026-04-22 18:33:33 +0300

    Update script.js
    blog website

--- script.js
+++ script.js
@@ -28,6 +28,17 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("darkToggle").onclick = () => {
     document.body.classList.toggle("dark");
   };
+  
+
+function toggleDrawer(){
+  document.getElementById("drawer").classList.add("open");
+  document.getElementById("overlay").classList.add("active");
+}
+
+function closeDrawer(){
+  document.getElementById("drawer").classList.remove("open");
+  document.getElementById("overlay").classList.remove("active");
+}
 
   // ===== TOKEN =====
   window.token = localStorage.getItem("token");
